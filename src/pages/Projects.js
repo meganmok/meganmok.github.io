749a19095d4cf1ddd5df0cb60cf6310187bd51c8
meganmok <92960807+meganmok@users.noreply.github.com>
2023-08-10 17:39:56 -0700

    allowed for clicking on projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -16,68 +16,28 @@ import travel from '../components/images/travel_app.png';
 import ec from '../components/images/espressco_app.png';
 
 function Projects() {
-  {/*const [isShown, setIsShown] = useState(false);
-  const [isShown2, setIsShown2] = useState(false);
-  const [isShown3, setIsShown3] = useState(false);
-  const [isShown4, setIsShown4] = useState(false);
-  const [isShown5, setIsShown5] = useState(false);
-
+  const [isShown, setIsShown] = useState(false);
   const handleClick = event => {
-    // 👇️ toggle shown state
     setIsShown(current => !current);
-    setIsShown2(false);
-    setIsShown3(false);
-    setIsShown4(false);
-    setIsShown5(false);
-    // 👇️ or simply set it to true
-    // setIsShown(true);
   };
-  const handleClick2 = event => {
-    // 👇️ toggle shown state
-    setIsShown2(current => !current);
-    setIsShown(false);
-    setIsShown3(false);
-    setIsShown4(false);
-    setIsShown5(false);
-    // 👇️ or simply set it to true
-    // setIsShown(true);
-  };
-  const handleClick3 = event => {
-    // 👇️ toggle shown state
-    setIsShown3(current => !current);
-    setIsShown(false);
-    setIsShown2(false);
-    setIsShown4(false);
-    setIsShown5(false);
-    // 👇️ or simply set it to true
-    // setIsShown(true);
-  };
-  const handleClick4 = event => {
-    // 👇️ toggle shown state
-    setIsShown4(current => !current);
-    setIsShown(false);
-    setIsShown2(false);
-    setIsShown3(false);
-    setIsShown5(false);
-    // 👇️ or simply set it to true
-    // setIsShown(true);
-  };
-  const handleClick5 = event => {
-    // 👇️ toggle shown state
-    setIsShown5(current => !current);
-    setIsShown(false);
-    setIsShown2(false);
-    setIsShown3(false);
-    setIsShown4(false);
-    // 👇️ or simply set it to true
-    // setIsShown(true);
-  };*/}
     return (
       <div id = "projects">
         <div id = "projectHeader">
         <p id = "projectTitle" className = "headerContent"> personal projects</p>
         </div>
         <div id = "projectMiddle">
+        {isShown && (<div>  
+    <FadeIn>
+    <p id = "shellCommand" className= "typewriter"> cat description.txt </p>
+    <p id = "shellDescription1" className= "shellText"> a C program that is designed to emulate the linux shell, by creating a shell environment for the user. 
+</p>
+<p id = "shellDescription2" className= "shellText"> implements basic directory functions, in addition to the incorporation of a directory stack. 
+</p>
+<p id = "shellDescription3" className= "shellText"> handles simple commands, the piping of multiple commands, as well as standard input and output redirection.
+</p>
+    <img src = {linux} id = "linux" alt = "linux" />
+    </FadeIn>
+</div>)}
         </div>
         <div id = "projectBottom">
         <div id = "designs">
@@ -103,7 +63,7 @@ function Projects() {
         <div id = "programs">
         <p className = "groupName"> programs</p>
         <div id = "progsContainer" className = "container"> 
-        <img src = {shell} className = "app" id = "shell" alt = "shell"/>
+        <img src = {shell} className = "app" id = "shell" alt = "shell" onClick = {handleClick}/>
         <img src = {apple} className = "app" id = "apple" alt = "apple"/>
         <img src = {canvas} className = "app" id = "canvas" alt = "canvas"/>
         </div>
@@ -113,56 +73,6 @@ function Projects() {
 
           </div>
     );
-    {/*
-        {isShown && (<div>  
-          <FadeIn>
-          <p id = "shellCommand" className= "typewriter"> cat description.txt </p>
-          <p id = "shellDescription1" className= "shellText"> a C program that is designed to emulate the linux shell, by creating a shell environment for the user. 
- </p>
- <p id = "shellDescription2" className= "shellText"> implements basic directory functions, in addition to the incorporation of a directory stack. 
- </p>
- <p id = "shellDescription3" className= "shellText"> handles simple commands, the piping of multiple commands, as well as standard input and output redirection.
- </p>
-          <img src = {linux} id = "linux" alt = "linux" />
-          </FadeIn>
-</div>)}
-        {isShown2 && (<div>  
-            <FadeIn>
-            <p id = "macCommand" className= "macTypewriter"> sh /Users/admin/macinit.sh </p>
-            <p id = "macDescription1" className= "macText"> A script run on macOS Monterey that accelerates the initialization process of Mac computers administered by the Phoenix Cluster at UC Davis. </p>
-            <p id = "macDescription2" className= "macText"> Provides hardware information about the Mac (such as Serial Number and IP Address) so that the device and its information can be easily updated in the Phoenix Cluster inventory system.</p>
-            <p id = "macDescription3" className= "macText"> Automates the installation of integral programs that lie within Microsoft Office 365, as well as backup and antimalware programs to ensure safety and security. </p>
-            <img src = {mac} id = "mac" alt = "mac" />
-             </FadeIn>
-        </div>)}
-        {isShown3 && (<div>  
-            <FadeIn>
-            <img src = {canvas_info} id = "canvas_info" alt = "canvas info" />
-             </FadeIn>
-        </div>)}
-        {isShown4 && (<div>  
-            <FadeIn>
-            <a href = "https://tiktokpets-stage-3-meganmok.ecs162-s22.repl.co/" target="_blank" rel="noopener noreferrer"> 
-            <img src = {tiktokpets} id = "tiktokpets" alt = "tiktok pets" /> 
-            </a>
-             </FadeIn>
-        </div>)}
-        {isShown5 && (<div>  
-            <FadeIn>
-            <a href = "https://www.figma.com/proto/7IxMljoy3vMJ3IHSPDq4Da/Hi-Fi-Prototypes?node-id=27%3A2918&starting-point-node-id=27%3A2918" target="_blank" rel="noopener noreferrer">
-            <img src = {igshop} id = "igshop" alt = "ig shop" /> 
-            </a>
-             </FadeIn>
-        </div>)}
-        <div id = "iconHolder">
-        <div id = "iconBar">
-        <img src = {shell} className = "app" id = "shell" alt = "shell" onClick = {handleClick}/>
-        <img src = {apple} className = "app" id = "apple" alt = "apple" onClick = {handleClick2}/>
-        <img src = {canvas} className = "app" id = "canvas" alt = "canvas" onClick = {handleClick3}/>
-        <img src = {shiba} className = "app" id = "shiba" alt = "shiba" onClick = {handleClick4}/>
-        <img src = {cart} className = "app" id = "cart" alt = "cart" onClick = {handleClick5}/>
-        </div>
-        </div> */}
 }
 
 export default Projects;
